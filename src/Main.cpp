d4db8a4862217e02fb0eea007215973aba874b37
fahadLine <152792332+fahadLine@users.noreply.github.com>
2025-09-14 17:40:40 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-// include the header file for the class Second
+
 using namespace std;
 
 int main() {
-    Second second;
-    cout << second.add(99, 1) << endl;
-    cout << second.sub(50.5, 10.5) << endl;
+    Calculator calc;
+    int a = 10, b = 5;
+
+    std::cout << "Addition: " << calc.add(a, b) << std::endl;
+    std::cout << "Subtraction: " << calc.subtract(a, b) << std::endl;
+    std::cout << "Multiplication: " << calc.multiply(a, b) << std::endl;
+    std::cout << "Division: " << calc.divide(a, b) << std::endl;
+
+    return 0;
 }
